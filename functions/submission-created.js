88ec45d48552af769dd1a5bc30b6b895d645ce90
yamag2 <y.yamaguchi.bz@gmail.com>
2020-11-18 23:37:01 +0900

    21231231232

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -2,4 +2,5 @@ exports.handler = function(event, context, callback) {
   console.log('event.body==>'+event.body);
   var payload = JSON.parse(unescape(event.body));
   console.log('payload==>'+payload);
+  console.log('payload[0]==>'+payload[0]);
 };
