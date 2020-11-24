8bf92a2cb2c5ba01828c01e970b2bf434a230f37
yamag2 <y.yamaguchi.bz@gmail.com>
2020-11-24 23:54:27 +0900

    GGG

--- src/.vuepress/components/NetlifyForm.vue
+++ src/.vuepress/components/NetlifyForm.vue
@@ -1,6 +1,7 @@
 <template>
 <form name="contact" method="POST" data-netlify="true">
   <input type="hidden" name="form-name" value="contact" />
+  <input type="hidden" name="articleno" value="134" />
   <p>
     <label>Your Name: <input type="text" name="name" /></label>   
   </p>
