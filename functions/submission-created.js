82874a4ae870bba8472cee55ed990b7bf87ecec3
yamag2 <y.yamaguchi.bz@gmail.com>
2020-11-24 23:41:43 +0900

    CCC

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 exports.handler = function(event, context, callback) {
 //  console.log('event.body==>'+event.body);
-  var payload = JSON.parse(event.body);
+  var payload = JSON.parse(event.body).payload;
   console.log('payload==>'+payload);
 //  console.log('payload[0]==>'+payload[0]);
 
