32f82a2b6f6db6e3ca9b8f6d14531a0fe1acfd68
yamag2 <y.yamaguchi.bz@gmail.com>
2020-11-26 19:53:46 +0900

    XXXXX

--- src/.vuepress/components/NetlifyForm.vue
+++ src/.vuepress/components/NetlifyForm.vue
@@ -1,4 +1,25 @@
 <template>
+
+
+<div id="app-5">
+  <p>{{ message }}</p>
+  <button v-on:click="reverseMessage">Reverse Message</button>
+</div>
+
+</form>
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+
 <form name="contact" method="POST" data-netlify="true">
   <input type="hidden" name="form-name" value="contact" />
   <input type="hidden" name="articleno" value="134" />
@@ -20,23 +41,4 @@
   <p>
     <button type="submit">Send</button>
   </p>
-
-<div id="app-5">
-  <p>{{ message }}</p>
-  <button v-on:click="reverseMessage">Reverse Message</button>
-</div>
-
-</form>
-var app5 = new Vue({
-  el: '#app-5',
-  data: {
-    message: 'Hello Vue.js!'
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    }
-  }
-})
-
 </template>
