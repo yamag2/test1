ea91a9ff0063b408ab55627347670cc1084febb4
yamag2 <y.yamaguchi.bz@gmail.com>
2020-11-26 19:44:43 +0900

    ddddddd

--- src/.vuepress/components/NetlifyForm.vue
+++ src/.vuepress/components/NetlifyForm.vue
@@ -22,3 +22,20 @@
   </p>
 </form>
 </template>
+
+<div id="app-5">
+  <p>{{ message }}</p>
+  <button v-on:click="reverseMessage">Reverse Message</button>
+</div>
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
