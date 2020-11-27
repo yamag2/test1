a1344580590187e4fe921b72a88482d009bd03f2
yamag2 <y.yamaguchi.bz@gmail.com>
2020-11-27 22:34:23 +0900

    XXXXXX

--- src/.vuepress/components/VueTest.vue
+++ src/.vuepress/components/VueTest.vue
@@ -1,30 +1,16 @@
 <template>
-  <div id="app">
+  <div id='app'>
+    <p>{{ message }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  el: '#app',
-  data () {
-    return {
-      message: 'Book List',
-      books: null,
-    }
-  },  
   mounted() {
-    fetch(
-      'https://script.google.com/macros/s/AKfycbwucscUpL0i2YlrbXosLWfAYGA9X6IigIKnNyW1wc1BxraDhQ/exec',
-    )
-    .then(res => res.json())
-    .then(
-      result => {
-        this.books = result
-      },
-      error => {
-
-      },
-    );
+	  el: '#app',
+	  data: {
+	    message: 'Hello VueJS!'
+	  }
   },
 } 
 </script>
